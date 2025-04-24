8cb8ea1661228d59a46fa502734af7a7fe0fce9e
Assassin33GT <bodeidris33@gmail.com>
2025-04-25 02:34:52 +0300

    chat

--- lib/supportChatPage.dart
+++ lib/supportChatPage.dart
@@ -49,7 +49,7 @@ class _SupportChatPageState extends State<SupportChatPage> {
         await _firestore
             .collection('chats_id')
             .where('chatId', isEqualTo: chatId)
-            .limit(0)
+            .limit(1)
             .get();
 
     // Only add if it doesn't already exist
