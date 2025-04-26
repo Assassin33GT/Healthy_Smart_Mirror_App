e82af70e5744da1ed9b42e712a5457c393f8691c
Assassin33GT <bodeidris33@gmail.com>
2025-04-27 00:34:47 +0300

    skin_analysis

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/android/assets/images/Intro.gif": "8cc9c337596440479e
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "502cc224cc7003be5cfb66986d5e0daa",
+"flutter_bootstrap.js": "331a0e378ee83acd4f5729ba924869a6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
