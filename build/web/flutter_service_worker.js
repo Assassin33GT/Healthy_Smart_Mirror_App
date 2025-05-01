8ac5aa48f1bda5cc181bd4a4e1e48ee8c44f3008
Assassin33GT <bodeidris33@gmail.com>
2025-05-01 23:50:31 +0300

    camera

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/android/assets/images/admin.jpg": "2042f58ed7a3ab7289
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "6964cf23f66516d5aa148749b060e9d7",
+"flutter_bootstrap.js": "c0422d47686bb22e52be274da63ddb27",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
