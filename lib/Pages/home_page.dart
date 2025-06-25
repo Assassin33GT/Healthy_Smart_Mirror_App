6b45400c0e623f99075d930bf170e574611ab166
Assassin33GT <bodeidris33@gmail.com>
2025-06-25 20:58:12 +0300

    bug_fix

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -14,8 +14,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
