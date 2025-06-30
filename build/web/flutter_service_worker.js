ffd42c6a59b3c20a1c7448a9879e6bbcdf50b9cf
Assassin33GT <bodeidris33@gmail.com>
2025-06-30 21:38:01 +0300

    bugs-fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -59,14 +59,14 @@ const RESOURCES = {"assets/android/assets/images/admin.jpg": "2042f58ed7a3ab7289
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "02ce3ac2bffcc371b7f8560d213ebce6",
+"flutter_bootstrap.js": "a190e9941545643490f821f90c173afe",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "502b97ddca5b2cd059c2d6f603ea7928",
 "/": "502b97ddca5b2cd059c2d6f603ea7928",
-"main.dart.js": "4023247a80c4485c8043873ff5dfbec4",
+"main.dart.js": "a3b074ff9380682da8dcd0deee1f3709",
 "manifest.json": "8836bc035984c10a2b3625e8bfe2a287",
 "version.json": "1e98a4f7390d2e9d9ad60c99fb0aff05"};
 // The application shell files that are downloaded before a service worker can
--- lib/Account_Buttons/forget_password_page.dart
+++ lib/Account_Buttons/forget_password_page.dart
@@ -1,35 +1,84 @@
+import 'package:demo/pages/login_page.dart';
 import 'package:demo/widgets/form_container_widget.dart';
 import 'package:demo/main.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class ForgetPasswordPage extends StatefulWidget{
+class ForgetPasswordPage extends StatefulWidget {
   const ForgetPasswordPage({super.key});
 
   @override
   State<ForgetPasswordPage> createState() => _ForgetPasswordPageState();
 }
 
-class _ForgetPasswordPageState extends State<ForgetPasswordPage>{
-
+class _ForgetPasswordPageState extends State<ForgetPasswordPage> {
   TextEditingController _emailController = TextEditingController();
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Change Password"),
-        backgroundColor: Colors.white,
+      backgroundColor: color1,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                color1 != Colors.black87
+                    ? color1
+                    : const Color.fromARGB(255, 85, 22, 96),
+                color2,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(24),
+              bottomRight: Radius.circular(24),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10,
+                offset: const Offset(0, 5),
+              ),
+            ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 20.0, left: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  child: Icon(Icons.arrow_back, color: Colors.white70),
+                ),
+                const SizedBox(width: 20,),
+                Text(
+                  "Forget Password!",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [
-              color1,
-              color2,
-            ],
+            colors: [color1, color2],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
@@ -48,32 +97,38 @@ class _ForgetPasswordPageState extends State<ForgetPasswordPage>{
                 FormContainerWidget(
                   controller: _emailController,
                   hintText: "Enter your email",
-                  
                 ),
                 SizedBox(height: 20),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
+                    backgroundColor:
+                        color1 != Colors.black87
+                            ? Colors.red
+                            : const Color.fromARGB(214, 151, 30, 21),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(13),
                     ),
                   ),
-                  onPressed: () async{
+                  onPressed: () async {
                     String email = _emailController.text.trim();
-                    if(email.isNotEmpty){
-                      try{
-                        await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text("Password reset link sent to $email"))
+                    if (email.isNotEmpty) {
+                      try {
+                        await FirebaseAuth.instance.sendPasswordResetEmail(
+                          email: email,
                         );
-                      }catch(e){
                         ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text("Please enter a valid email"))
+                          SnackBar(
+                            content: Text("Password reset link sent to $email"),
+                          ),
+                        );
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Please enter a valid email")),
                         );
                       }
-                    }else{
+                    } else {
                       ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text("Please enter your email"))
+                        SnackBar(content: Text("Please enter your email")),
                       );
                     }
                   },
@@ -83,7 +138,7 @@ class _ForgetPasswordPageState extends State<ForgetPasswordPage>{
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                     ),
-                    ),
+                  ),
                 ),
               ],
             ),
